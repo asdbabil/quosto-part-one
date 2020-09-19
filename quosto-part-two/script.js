fa6905ece998715745e207d9305c397105bf7e2d
Sany <asdbabil@users.noreply.github.com>
2020-09-18 20:31:30 -0400

    fixing bug

--- quosto-part-two/script.js
+++ quosto-part-two/script.js
@@ -4,24 +4,17 @@ let inches = "*"
 console.log("Rainfall: " + inches.repeat(rain));
 
 let yield = 50
-if (rain >= 20) { 
-    yield = yield*.9
-}
-if (rain < 10) {
-    yield = yield*.8
-}
+if (rain >= 20)
+    yield -= yield*.1
+else if (rain < 10)
+    yield -= yield*.2
 
 let fertilizer = prompt("Did you use fertilizer?");
-if (fertilizer === "no") {
-    console.log("The yield should be " + yield + " bushels per acre.")
-}
 if (fertilizer === "yes") {
     var type = prompt("Did you use premium or regular fertilizer?")
+    if (type === "premium")
+        yield = yield*.15;
+    else if (type === "regular")
+        yield = yield*.10;
 }
-
-if (type === "premium") {
-    yield = yield*1.15;
-    console.log("The yield should be " + yield + "bushels per acre.")
-} else {
-    yield = yield*1.10
-}
+console.log("The yield should be " + yield + " bushels per acre.")
